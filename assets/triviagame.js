8bdc6ac67d9064c301ee1a23203a61b617ebc7da
Grant Chan <grant.r.chan@gmail.com>
2017-07-17 23:53:18 -0500

    First update

---
+++ assets/triviagame.js
@@ -0,0 +1,44 @@
+//Hide all other screens at the start
+$('#gameGUI').hide();
+$('#gameOverScreen').hide();
+
+	var QsBank = {
+		Q1: "Which of the following exercises train the deltoids?",
+		Q2: "What do DOMS mean in strength training?",
+		Q3: "Where is the Latissimus dorsi located?",
+		Q4: "The human body is capable of movement in which three planes of motion?",
+		Q5: "What is the largest muscle in the human body?"
+	};
+
+
+	var AnsBank = {
+		A1: ["Chest Press", "Squats", "Deadlifts", "Shoulder Press"],
+		A2: ["Deadlifts Offer More Swole", "Document Object Model Suite", "Delayed Optimal Muscle Swoleness",
+		"Delayed Onset Muscle Soreness" ],
+		A3: ["In the back", "In the face", "In the arms", "In the butt"],
+		A4: ["Top, Middle, Bottom", "Left, Center, Right", "Coronal, Transverse, Sagittal", "Polar, Radial, Azimuthal"]
+		A5: ["Gluteus Maximus", "Pectoralis major", "Biceps brachii", "Rectus abdominis"]
+
+	};
+
+function Quizzer(){
+	$("#Qsdiv").html(QsBank.Q1);
+	AnsBank.A1[0].delay(3000);
+	AnsBank.A1[0].fadeIn(3000);
+	AnsBank.A1[1].fadeIn(500);
+	AnsBank.A1[2].fadeIn(500);
+	AnsBank.A1[3].fadeIn(500);
+
+}
+
+
+
+document.onkeyup = function(event){
+		titlepagelock = true;
+		$('#titlepage').slideUp(1500);
+		$('#gameGUI').delay(1500);
+		$('#gameGUI').slideDown(1500);
+		Quizzer();
+	}
+
+
